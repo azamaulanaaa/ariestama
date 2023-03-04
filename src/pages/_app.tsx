4e7e87d9a7ba62e86f42bf5cd69ea1c968c138e5
Aza Maulana <azamaulanaaa@gmail.com>
2023-03-05 00:06:26 +0700

    feat: install tailwind and supabase

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import {
+    createBrowserSupabaseClient,
+    Session,
+} from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+import '../global.css';
+
+function MyApp({
+    Component,
+    pageProps,
+}: AppProps<{ initialSession: Session }>) {
+    const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+    return (
+        <SessionContextProvider
+            supabaseClient={supabaseClient}
+            initialSession={pageProps.initialSession}
+        >
+            <Component {...pageProps} />
+        </SessionContextProvider>
+    );
+}
+
+export default MyApp;
