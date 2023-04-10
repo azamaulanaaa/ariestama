463f11791072929947ba475827b5e09fb5dd510a
Aza Maulana <azamaulanaaa@gmail.com>
2023-04-10 16:38:49 +0700

    feat!: use material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: colors.cyan[300],
+                scondary: colors.rose[300],
+                error: colors.red[500],
+                success: colors.green[500],
+            },
+        },
     },
     plugins: [],
-};
+});
