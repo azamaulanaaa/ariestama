b36b0502fe7d5beeebed0ca541da62de8ba6b74b
Aza Maulana <azamaulanaaa@gmail.com>
2023-04-11 15:03:16 +0700

    feat(components): add signup form

--- src/components/form/SignUp/index.test.tsx
+++ src/components/form/SignUp/index.test.tsx
@@ -1,12 +1,14 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SignUp from '.';
 
-afterEach(() => {
-    jest.resetAllMocks();
-    cleanup();
-});
-
 describe('SignUp Form Component', () => {
+    const user = userEvent.setup();
+    afterEach(() => {
+        jest.resetAllMocks();
+        cleanup();
+    });
+
     it('default render', () => {
         render(<SignUp />);
 
@@ -14,10 +16,161 @@ describe('SignUp Form Component', () => {
         screen.getByRole('form');
         screen.getByLabelText(/email/i);
         screen.getByLabelText(/^password/i);
-        screen.getByLabelText(/confirm Password/i);
+        screen.getByLabelText(/confirm password/i);
         const btn_submit = screen.getByRole('button');
 
         expect(title).toHaveTextContent('Sign up');
         expect(btn_submit).toHaveTextContent(/submit/i);
+    });
+
+    it('call onSubmit on click submit botton', async () => {
+        const handleSubmit = jest.fn();
+        const data = {
+            email: 'some@email.com',
+            password: 'somesome',
+        };
+
+        render(<SignUp onSubmit={handleSubmit} />);
+
+        const input_email = screen.getByLabelText(/email/i);
+        const input_password = screen.getByLabelText(/^password/i);
+        const input_confirm_password =
+            screen.getByLabelText(/confirm password/i);
+        const btn_submit = screen.getByRole('button');
+
+        await user.type(input_email, data.email);
+        await user.type(input_password, data.password);
+        await user.type(input_confirm_password, data.password);
+        await user.click(btn_submit);
+
+        await waitFor(() => {
+            expect(handleSubmit).toBeCalledTimes(1);
+            expect(handleSubmit).toBeCalledWith(data);
+        });
+    });
+
+    it('support error message', async () => {
+        const message = 'some';
+
+        render(<SignUp alertProps={{ color: 'red', children: message }} />);
+
+        await waitFor(() => {
+            const alert = screen.getByRole('alert');
+            expect(alert).toHaveTextContent(message);
+        });
+    });
+
+    it('support success message', async () => {
+        const message = 'some';
+
+        render(<SignUp alertProps={{ color: 'green', children: message }} />);
+
+        await waitFor(() => {
+            const alert = screen.getByRole('alert');
+            expect(alert).toHaveTextContent(message);
+        });
+    });
+
+    it('everytimes password change make sure password and confirm password are the same', async () => {
+        render(<SignUp />);
+
+        const input_password = screen.getByLabelText(/^password/i);
+        const input_confirm_password =
+            screen.getByLabelText(/confirm password/i);
+
+        expect(input_confirm_password).toBeInvalid();
+        await user.type(input_password, 'some');
+        await waitFor(() => {
+            expect(input_confirm_password).toBeInvalid();
+        });
+        await user.type(input_confirm_password, 'some2');
+        await user.type(input_password, '2');
+        await waitFor(() => {
+            expect(input_confirm_password).toBeValid();
+        });
+    });
+
+    it('everytimes confirm password change make sure password and confirm password are the same', async () => {
+        render(<SignUp />);
+
+        const input_password = screen.getByLabelText(/^password/i);
+        const input_confirm_password =
+            screen.getByLabelText(/confirm password/i);
+
+        expect(input_confirm_password).toBeInvalid();
+        await user.type(input_confirm_password, 'some');
+        await waitFor(() => {
+            expect(input_confirm_password).toBeInvalid();
+        });
+        await user.type(input_password, 'some2');
+        await user.type(input_confirm_password, '2');
+        await waitFor(() => {
+            expect(input_confirm_password).toBeValid();
+        });
+    });
+
+    it('valid password should have minimum char of 6', async () => {
+        render(<SignUp />);
+
+        const input_password = screen.getByLabelText(/^password/i);
+        expect(input_password).toBeInvalid();
+
+        for (let i = 0; i < 5; i++) {
+            await user.type(input_password, 'w');
+            await waitFor(() => {
+                expect(input_password).toBeInvalid();
+            });
+        }
+
+        await user.type(input_password, 'w');
+        await waitFor(() => {
+            expect(input_password).toBeValid();
+        });
+    });
+
+    it('set email to invalid if inputed with invalid email', async () => {
+        const invalidEmails = [
+            'some',
+            'some@',
+            'some@email',
+            'some @email.com',
+        ];
+
+        render(<SignUp />);
+
+        const input_email = screen.getByLabelText(/email/i);
+
+        expect(input_email).toBeInvalid();
+
+        invalidEmails.forEach(async (invalidEmail) => {
+            await user.clear(input_email);
+            await user.type(input_email, invalidEmail);
+            await waitFor(() => {
+                expect(input_email).toBeInvalid();
+            });
+        });
+    });
+
+    it('set email to valid if inputed with valid email', async () => {
+        const validEmails = [
+            'some@gmail.com',
+            'some@example.com',
+            's0m33@yahoo.to',
+            'n4m3_e@domain.wow',
+        ];
+
+        render(<SignUp />);
+
+        const input_email = screen.getByLabelText(/email/i);
+
+        expect(input_email).toBeInvalid();
+
+        validEmails.forEach(async (validEmail) => {
+            await user.clear(input_email);
+            await user.type(input_email, validEmail);
+            await waitFor(() => {
+                expect(input_email).toBeValid();
+            });
+        });
     });
 });
--- src/components/form/SignUp/index.tsx
+++ src/components/form/SignUp/index.tsx
@@ -1,38 +1,133 @@
-import { Alert, Button, Input, Typography } from '@material-tailwind/react';
+import {
+    Alert,
+    AlertProps,
+    Button,
+    Input,
+    Typography,
+} from '@material-tailwind/react';
+import { FormEvent, ChangeEvent, useRef, useState } from 'react';
 
-export interface SignUpProps {}
+export interface SignUpProps {
+    onSubmit?: (data: SignUpData) => void;
+    alertProps?: AlertProps;
+}
+
+export interface SignUpData {
+    email: string;
+    password: string;
+}
 
 const SignUp = (props: SignUpProps) => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+    const [passwordMatch, setPasswordMatch] = useState<boolean>(true);
+    const [passwordEnoughLength, setPasswordEnoughLength] =
+        useState<boolean>(true);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (
+            !props.onSubmit ||
+            !emailRef.current ||
+            !passwordRef.current ||
+            !confirmPasswordRef.current
+        )
+            return;
+
+        if (!validatePassword) return;
+
+        props.onSubmit({
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        });
+    };
+
+    const validatePassword = () => {
+        if (!passwordRef.current || !confirmPasswordRef.current) return;
+
+        if (passwordRef.current.value.length < 6) {
+            setPasswordEnoughLength(false);
+            passwordRef.current.setCustomValidity(
+                'Password length is less than 6'
+            );
+        } else {
+            setPasswordEnoughLength(true);
+            passwordRef.current.setCustomValidity('');
+        }
+
+        if (passwordRef.current.value != confirmPasswordRef.current.value) {
+            setPasswordMatch(false);
+            confirmPasswordRef.current.setCustomValidity(
+                'Confirm Password does not match to Password'
+            );
+        } else {
+            setPasswordMatch(true);
+            confirmPasswordRef.current.setCustomValidity('');
+        }
+
+        return passwordEnoughLength && passwordMatch;
+    };
+
+    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+        validatePassword();
+    };
+
+    const FormAlert = () => {
+        if (!props.alertProps) return null;
+        return <Alert {...props.alertProps} />;
+    };
+
     return (
-        <form role="form">
-            <div className="flex flex-col gap-4">
-                <Typography variant="h2">Sign up</Typography>
+        <form role="form" onSubmit={handleSubmit}>
+            <Typography variant="h4" component="h1">
+                Sign up
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+                to have account in Ariestama System
+            </Typography>
+            <div className="flex flex-col gap-6 mt-6">
+                <FormAlert />
                 <Input
+                    inputRef={emailRef}
                     id="email"
                     name="email"
                     label="Email"
                     labelProps={{ htmlFor: 'email' }}
                     type="email"
                     required
+                    size="lg"
                 />
                 <Input
+                    inputRef={passwordRef}
                     id="password"
                     name="password"
                     label="Password"
                     labelProps={{ htmlFor: 'password' }}
                     type="password"
                     required
+                    minLength={6}
+                    onInput={handlePasswordChange}
+                    error={!passwordEnoughLength}
+                    size="lg"
                 />
                 <Input
+                    inputRef={confirmPasswordRef}
                     id="confirm-password"
                     name="confirm-password"
                     label="Confirm Password"
                     labelProps={{ htmlFor: 'confirm-password' }}
                     type="password"
                     required
+                    onInput={handlePasswordChange}
+                    error={!passwordMatch}
+                    size="lg"
                 />
                 <div className="col-span-2">
-                    <Button type="submit">Submit</Button>
+                    <Button type="submit" ripple={false} fullWidth>
+                        Submit
+                    </Button>
                 </div>
             </div>
         </form>
