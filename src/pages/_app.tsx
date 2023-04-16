12817eb8a38f23d4581942d513a5edad72c47165
Aza Maulana <azamaulanaaa@gmail.com>
2023-04-16 11:55:38 +0700

    fix(pages)!: _app.tsx is not updated with new Database lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@/components/SessionContext';
 
 import '../global.css';
+import Database from '@/libs/Database';
 
 function MyApp({ Component, pageProps }: AppProps) {
     const supabaseClient = createBrowserSupabaseClient();
+    const database = new Database(supabaseClient);
 
     return (
-        <SessionContextProvider supabaseClient={supabaseClient}>
+        <SessionContextProvider database={database}>
             <Component {...pageProps} />
         </SessionContextProvider>
     );
