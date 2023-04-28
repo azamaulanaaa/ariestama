46abbdd5063ea4b6ff95ee96634854695ce6cdd0
Aza Maulana <azamaulanaaa@gmail.com>
2023-04-28 08:07:03 +0700

    chores(test): fix typo

--- src/components/UnitsTable/index.test.tsx
+++ src/components/UnitsTable/index.test.tsx
@@ -2,7 +2,7 @@ import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UnitsTable from '.';
 
-describe('Unit Table Component', () => {
+describe('Units Table Component', () => {
     const user = userEvent.setup();
     afterEach(() => {
         cleanup();
