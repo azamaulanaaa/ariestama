a98c22557a0c71d27c3bbcbadd8a9cd3db7768aa
Aza Maulana <azamaulanaaa@gmail.com>
2023-05-01 08:52:19 +0700

    test(pages): add mocks for ripple effects

--- src/__tests__/signin.test.tsx
+++ src/__tests__/signin.test.tsx
@@ -20,6 +20,13 @@ jest.mock('@/components/SessionContext', () => ({
     },
 }));
 
+jest.mock('material-ripple-effects', () => ({
+    __esModule: true,
+    default: () => ({
+        create() {},
+    }),
+}));
+
 describe('SignIn Page', () => {
     afterEach(() => {
         cleanup();
