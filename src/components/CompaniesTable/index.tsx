3a4f44f6a501b8c419328e87e251e7f4709ce3ab
Aza Maulana <azamaulanaaa@gmail.com>
2023-05-14 13:35:40 +0700

    feat(components): add companies table

---
+++ src/components/CompaniesTable/index.tsx
@@ -0,0 +1,28 @@
+import Table, { TableData } from '@/components/Table';
+
+export interface CompaniesItemData {
+    name: string;
+    branch: string;
+    city: string;
+    province: string;
+}
+
+export interface CompaniesTableProps {
+    items: CompaniesItemData[];
+    onClick?: (itemData: TableData<keyof CompaniesItemData>) => void;
+}
+
+const CompaniesTable = (props: CompaniesTableProps) => {
+    const headers = {
+        name: 'Name',
+        branch: 'Branch',
+        city: 'City',
+        province: 'Province',
+    };
+
+    return (
+        <Table headers={headers} items={props.items} onClick={props.onClick} />
+    );
+};
+
+export default CompaniesTable;
