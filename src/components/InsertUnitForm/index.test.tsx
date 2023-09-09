bd05d63d96c15d09756c24deac3804cb089c084a
Aza Maulana <azamaulanaaa@gmail.com>
2023-09-10 00:51:54 +0700

    feat(components): implement unit's series to InsertUnitForm

--- src/components/InsertUnitForm/index.test.tsx
+++ src/components/InsertUnitForm/index.test.tsx
@@ -1,126 +1,137 @@
-import { cleanup, render, screen } from '@testing-library/react';
-import UserEvent from '@testing-library/user-event';
-import InsertUnitForm from '.';
+import { cleanup, render, screen } from "@testing-library/react";
+import UserEvent from "@testing-library/user-event";
+import InsertUnitForm from ".";
 
-jest.mock('material-ripple-effects', () => ({
-    __esModule: true,
-    default: () => ({
-        create() {},
-    }),
+jest.mock("material-ripple-effects", () => ({
+  __esModule: true,
+  default: () => ({
+    create() {},
+  }),
 }));
 
-describe('InsertUnitForm Component', () => {
-    afterEach(cleanup);
+describe("InsertUnitForm Component", () => {
+  afterEach(cleanup);
 
-    it('render a complete form', () => {
-        render(<InsertUnitForm />);
+  it("render a complete form", () => {
+    render(<InsertUnitForm />);
 
-        screen.getByRole('form');
-        screen.getByRole('button');
-        screen.getByLabelText(/serial number/i);
-        screen.getByLabelText(/brand/i);
-        screen.getByLabelText(/original equipment manufacture/i);
-        screen.getByLabelText(/year of manufacture/i);
-        screen.getByLabelText(/made in/i);
+    screen.getByRole("form");
+    screen.getByRole("button");
+    screen.getByLabelText(/serial number/i);
+    screen.getByLabelText(/brand/i);
+    screen.getByLabelText(/original equipment manufacture/i);
+    screen.getByLabelText(/year of manufacture/i);
+    screen.getByLabelText(/made in/i);
+  });
+
+  it("call onSubmit function if submit button is clicked", async () => {
+    const testdata = {
+      serial_number: "serial number",
+      series: "series",
+      brand: "brand",
+      oem: "oem",
+      yom: 2000,
+      made_in: "made_in",
+    };
+    const handleSubmit = jest.fn();
+    const user = UserEvent.setup();
+
+    render(<InsertUnitForm onSubmit={handleSubmit} />);
+    const button = screen.getByRole("button");
+    const input_serial_number = screen.getByLabelText(/serial number/i);
+    const input_series = screen.getByLabelText(/series/i);
+    const input_brand = screen.getByLabelText(/brand/i);
+    const input_oem = screen.getByLabelText(/original equipment manufacture/i);
+    const input_yom = screen.getByLabelText(/year of manufacture/i);
+    const input_made_in = screen.getByLabelText(/made in/i);
+
+    await user.type(input_serial_number, testdata.serial_number);
+    await user.type(input_series, testdata.series);
+    await user.type(input_brand, testdata.brand);
+    await user.type(input_oem, testdata.oem);
+    await user.type(input_yom, String(testdata.yom));
+    await user.type(input_made_in, testdata.made_in);
+
+    expect(handleSubmit).toBeCalledTimes(0);
+
+    await user.click(button);
+    expect(handleSubmit).toBeCalledTimes(1);
+    expect(handleSubmit).toBeCalledWith({
+      serial_number: testdata.serial_number,
+      series: testdata.series,
+      brand: testdata.brand,
+      oem: testdata.oem,
+      yom: testdata.yom,
+      made_in: testdata.made_in,
     });
+  });
 
-    it('call onSubmit function if submit button is clicked', async () => {
-        const testdata = {
-            serial_number: 'serial number',
-            brand: 'brand',
-            oem: 'oem',
-            yom: 2000,
-            made_in: 'made_in',
-        };
-        const handleSubmit = jest.fn();
-        const user = UserEvent.setup();
+  it("input serial number cannot be empty", async () => {
+    const user = UserEvent.setup();
 
-        render(<InsertUnitForm onSubmit={handleSubmit} />);
-        const button = screen.getByRole('button');
-        const input_serial_number = screen.getByLabelText(/serial number/i);
-        const input_brand = screen.getByLabelText(/brand/i);
-        const input_oem = screen.getByLabelText(
-            /original equipment manufacture/i
-        );
-        const input_yom = screen.getByLabelText(/year of manufacture/i);
-        const input_made_in = screen.getByLabelText(/made in/i);
+    render(<InsertUnitForm />);
+    const input_serial_number = screen.getByLabelText(/serial number/i);
 
-        await user.type(input_serial_number, testdata.serial_number);
-        await user.type(input_brand, testdata.brand);
-        await user.type(input_oem, testdata.oem);
-        await user.type(input_yom, String(testdata.yom));
-        await user.type(input_made_in, testdata.made_in);
+    expect(input_serial_number).toBeInvalid();
+    await user.type(input_serial_number, "serial number");
+    expect(input_serial_number).toBeValid();
+  });
 
-        expect(handleSubmit).toBeCalledTimes(0);
+  it("input series cannot be empety", async () => {
+    const user = UserEvent.setup();
 
-        await user.click(button);
-        expect(handleSubmit).toBeCalledTimes(1);
-        expect(handleSubmit).toBeCalledWith({
-            serial_number: testdata.serial_number,
-            brand: testdata.brand,
-            oem: testdata.oem,
-            yom: testdata.yom,
-            made_in: testdata.made_in,
-        });
-    });
+    render(<InsertUnitForm />);
+    const input_series = screen.getByLabelText(/series/i);
 
-    it('input serial number cannot be empty', async () => {
-        const user = UserEvent.setup();
+    expect(input_series).toBeInvalid();
+    await user.type(input_series, "series");
+    expect(input_series).toBeValid();
+  });
 
-        render(<InsertUnitForm />);
-        const input_serial_number = screen.getByLabelText(/serial number/i);
+  it("input brand cannot be empty", async () => {
+    const user = UserEvent.setup();
 
-        expect(input_serial_number).toBeInvalid();
-        await user.type(input_serial_number, 'serial number');
-        expect(input_serial_number).toBeValid();
-    });
+    render(<InsertUnitForm />);
+    const input_brand = screen.getByLabelText(/brand/i);
 
-    it('input brand cannot be empty', async () => {
-        const user = UserEvent.setup();
+    expect(input_brand).toBeInvalid();
+    await user.type(input_brand, "brand");
+    expect(input_brand).toBeValid();
+  });
 
-        render(<InsertUnitForm />);
-        const input_brand = screen.getByLabelText(/brand/i);
+  it("input oem cannot be empty", async () => {
+    const user = UserEvent.setup();
 
-        expect(input_brand).toBeInvalid();
-        await user.type(input_brand, 'brand');
-        expect(input_brand).toBeValid();
-    });
+    render(<InsertUnitForm />);
+    const input_oem = screen.getByLabelText(/original equipment manufacture/i);
 
-    it('input oem cannot be empty', async () => {
-        const user = UserEvent.setup();
+    expect(input_oem).toBeInvalid();
+    await user.type(input_oem, "oem");
+    expect(input_oem).toBeValid();
+  });
 
-        render(<InsertUnitForm />);
-        const input_oem = screen.getByLabelText(
-            /original equipment manufacture/i
-        );
+  it("input yom cannot be empty and must be a number", async () => {
+    const user = UserEvent.setup();
 
-        expect(input_oem).toBeInvalid();
-        await user.type(input_oem, 'oem');
-        expect(input_oem).toBeValid();
-    });
+    render(<InsertUnitForm />);
+    const input_yom = screen.getByLabelText(/year of manufacture/i);
 
-    it('input yom cannot be empty and must be a number', async () => {
-        const user = UserEvent.setup();
+    expect(input_yom).toBeInvalid();
+    await user.type(input_yom, "yom");
+    expect(input_yom).toBeInvalid();
+    await user.clear(input_yom);
+    await user.type(input_yom, "60000");
+    expect(input_yom).toBeValid();
+  });
 
-        render(<InsertUnitForm />);
-        const input_yom = screen.getByLabelText(/year of manufacture/i);
+  it("input made in cannot be empty", async () => {
+    const user = UserEvent.setup();
 
-        expect(input_yom).toBeInvalid();
-        await user.type(input_yom, 'yom');
-        expect(input_yom).toBeInvalid();
-        await user.clear(input_yom);
-        await user.type(input_yom, '60000');
-        expect(input_yom).toBeValid();
-    });
+    render(<InsertUnitForm />);
+    const input_made_in = screen.getByLabelText(/made in/i);
 
-    it('input made in cannot be empty', async () => {
-        const user = UserEvent.setup();
-
-        render(<InsertUnitForm />);
-        const input_made_in = screen.getByLabelText(/made in/i);
-
-        expect(input_made_in).toBeInvalid();
-        await user.type(input_made_in, 'made in');
-        expect(input_made_in).toBeValid();
-    });
+    expect(input_made_in).toBeInvalid();
+    await user.type(input_made_in, "made in");
+    expect(input_made_in).toBeValid();
+  });
 });
--- src/components/InsertUnitForm/index.tsx
+++ src/components/InsertUnitForm/index.tsx
@@ -1,99 +1,111 @@
-import { Button, Input } from '@material-tailwind/react';
-import { FormEvent } from 'react';
+import { Button, Input } from "@material-tailwind/react";
+import { FormEvent } from "react";
 
 export type InsertUnitData = {
-    serial_number: string;
-    brand: string;
-    oem: string;
-    yom: number;
-    made_in: string;
+  serial_number: string;
+  series: string;
+  brand: string;
+  oem: string;
+  yom: number;
+  made_in: string;
 };
 
 export type InsertUnitFormProps = {
-    onSubmit?: (data: InsertUnitData) => void;
+  onSubmit?: (data: InsertUnitData) => void;
 };
 
 const InsertUnitForm = (props: InsertUnitFormProps) => {
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (!props.onSubmit) return;
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!props.onSubmit) return;
 
-        const form = event.currentTarget;
-        const form_data = new FormData(form);
+    const form = event.currentTarget;
+    const form_data = new FormData(form);
 
-        const serial_number_form = form_data.get('serial_number');
-        const brand_form = form_data.get('brand');
-        const oem_form = form_data.get('oem');
-        const yom_form = form_data.get('yom');
-        const made_in_form = form_data.get('made_in');
+    const serial_number_form = form_data.get("serial_number");
+    const series_form = form_data.get("series");
+    const brand_form = form_data.get("brand");
+    const oem_form = form_data.get("oem");
+    const yom_form = form_data.get("yom");
+    const made_in_form = form_data.get("made_in");
 
-        if (
-            !serial_number_form ||
-            !brand_form ||
-            !oem_form ||
-            !yom_form ||
-            !made_in_form
-        )
-            return;
+    if (
+      !serial_number_form ||
+      !series_form ||
+      !brand_form ||
+      !oem_form ||
+      !yom_form ||
+      !made_in_form
+    )
+      return;
 
-        props.onSubmit({
-            serial_number: serial_number_form.toString(),
-            brand: brand_form.toString(),
-            oem: oem_form.toString(),
-            yom: Number(yom_form.toString()),
-            made_in: made_in_form.toString(),
-        });
-    };
+    props.onSubmit({
+      serial_number: serial_number_form.toString(),
+      series: series_form.toString(),
+      brand: brand_form.toString(),
+      oem: oem_form.toString(),
+      yom: Number(yom_form.toString()),
+      made_in: made_in_form.toString(),
+    });
+  };
 
-    return (
-        <form role="form" onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-4">
-                <Input
-                    id="serial_number"
-                    name="serial_number"
-                    type="text"
-                    label="Serial Number"
-                    labelProps={{ htmlFor: 'serial_number' }}
-                    required
-                />
-                <Input
-                    id="brand"
-                    name="brand"
-                    type="text"
-                    label="Brand"
-                    labelProps={{ htmlFor: 'brand' }}
-                    required
-                />
-                <Input
-                    id="oem"
-                    name="oem"
-                    type="text"
-                    label="Original Equipment Manufacture"
-                    labelProps={{ htmlFor: 'oem' }}
-                    required
-                />
-                <Input
-                    id="yom"
-                    name="yom"
-                    type="number"
-                    label="Year of Manufacture"
-                    labelProps={{ htmlFor: 'yom' }}
-                    required
-                />
-                <Input
-                    id="made_in"
-                    name="made_in"
-                    type="text"
-                    label="Made In"
-                    labelProps={{ htmlFor: 'made_in' }}
-                    required
-                />
-                <Button type="submit" variant="gradient" className="col-span-2">
-                    Submit
-                </Button>
-            </div>
-        </form>
-    );
+  return (
+    <form role="form" onSubmit={handleSubmit}>
+      <div className="grid grid-cols-2 gap-4">
+        <Input
+          id="serial_number"
+          name="serial_number"
+          type="text"
+          label="Serial Number"
+          labelProps={{ htmlFor: "serial_number" }}
+          required
+        />
+        <Input
+          id="series"
+          name="series"
+          type="text"
+          label="Series"
+          labelProps={{ htmlFor: "series" }}
+          required
+        />
+        <Input
+          id="brand"
+          name="brand"
+          type="text"
+          label="Brand"
+          labelProps={{ htmlFor: "brand" }}
+          required
+        />
+        <Input
+          id="oem"
+          name="oem"
+          type="text"
+          label="Original Equipment Manufacture"
+          labelProps={{ htmlFor: "oem" }}
+          required
+        />
+        <Input
+          id="yom"
+          name="yom"
+          type="number"
+          label="Year of Manufacture"
+          labelProps={{ htmlFor: "yom" }}
+          required
+        />
+        <Input
+          id="made_in"
+          name="made_in"
+          type="text"
+          label="Made In"
+          labelProps={{ htmlFor: "made_in" }}
+          required
+        />
+        <Button type="submit" variant="gradient" className="col-span-2">
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default InsertUnitForm;
