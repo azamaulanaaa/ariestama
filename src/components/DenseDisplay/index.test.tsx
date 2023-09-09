c457c144dcda9e8bdfaaa2b9d747decd3fee81bc
Aza Maulana <azamaulanaaa@gmail.com>
2023-09-09 19:20:55 +0700

    feat(components)!: key and valaue are sperated in DenseDisplay
    previously DenseDisplay uses fields key as printed key. this might raise
    many issues such as hard to masking object data and difficulty to do
    integration test.
    
    in this commit we introduce keys and values props. keys prop holds
    printed key while values holds printed values. this changes also
    introduce generated data-testid attribute.

--- src/components/DenseDisplay/index.test.tsx
+++ src/components/DenseDisplay/index.test.tsx
@@ -10,40 +10,52 @@ describe("DenseDisplay Component", () => {
   it("render all fields for row variant", () => {
     const testdata: DenseDisplayProps<string> = {
       variant: "row",
-      data: {
+      keys: {
+        key1: "key1",
+        key2: "key2",
+        key3: "key 3",
+      },
+      values: {
         key1: "key11",
         key2: "key22",
-        "key 3": "key33",
+        key3: "key33",
       },
     };
 
     render(<DenseDisplay {...testdata} />);
-    const keys = screen.getAllByTestId("key");
-    const values = screen.getAllByTestId("value");
 
-    Object.entries(testdata.data).forEach(([key, value], index) => {
-      expect(keys[index]).toHaveTextContent(key);
-      expect(values[index]).toHaveTextContent(String(value));
+    Object.keys(testdata.keys).forEach((key) => {
+      const key_tag = screen.getByTestId("key_" + key);
+      const value_tag = screen.getByTestId("value_" + key);
+
+      expect(key_tag).toHaveTextContent(String(testdata.keys[key]));
+      expect(value_tag).toHaveTextContent(String(testdata.values[key]));
     });
   });
 
   it("render all fields for column variant", () => {
     const testdata: DenseDisplayProps<string> = {
       variant: "column",
-      data: {
+      keys: {
+        key1: "key1",
+        key2: "key2",
+        key3: "key 3",
+      },
+      values: {
         key1: "key11",
         key2: "key22",
-        "key 3": "key33",
+        key3: "key33",
       },
     };
 
     render(<DenseDisplay {...testdata} />);
-    const keys = screen.getAllByTestId("key");
-    const values = screen.getAllByTestId("value");
 
-    Object.entries(testdata.data).forEach(([key, value], index) => {
-      expect(keys[index]).toHaveTextContent(key);
-      expect(values[index]).toHaveTextContent(String(value));
+    Object.keys(testdata.keys).forEach((key) => {
+      const key_tag = screen.getByTestId("key_" + key);
+      const value_tag = screen.getByTestId("value_" + key);
+
+      expect(key_tag).toHaveTextContent(String(testdata.keys[key]));
+      expect(value_tag).toHaveTextContent(String(testdata.values[key]));
     });
   });
 });
--- src/components/DenseDisplay/index.tsx
+++ src/components/DenseDisplay/index.tsx
@@ -8,7 +8,8 @@ export type DenseDisplayVariant = "row" | "column";
 
 export type DenseDisplayProps<K extends string> = {
   variant?: DenseDisplayVariant;
-  data: DenseDisplayData<K>;
+  keys: DenseDisplayData<K>;
+  values: DenseDisplayData<K>;
 };
 
 const DenseDisplay = (props: DenseDisplayProps<string>) => {
@@ -17,19 +18,22 @@ const DenseDisplay = (props: DenseDisplayProps<string>) => {
       return (
         <table className="w-full">
           <tbody className="flex flex-wrap">
-            {Object.entries(props.data).map(([key, value]) => (
-              <tr className="flex grow-0 shrink flex-col lg:basis-1/3 md:basis-1/2 sm:basis-full border-b-[1px]">
+            {Object.entries(props.keys).map(([key, value]) => (
+              <tr
+                key={key}
+                className="flex grow-0 shrink flex-col lg:basis-1/3 md:basis-1/2 sm:basis-full border-b-[1px]"
+              >
                 <th
-                  data-testid="key"
+                  data-testid={"key_" + key}
                   className="w-full text-left font-normal text-gray-500"
                 >
-                  {key}
+                  {value}
                 </th>
                 <td
-                  data-testid="value"
+                  data-testid={"value_" + key}
                   className="flex grow shrink text-left font-normal"
                 >
-                  {value}
+                  {props.values[key]}
                 </td>
               </tr>
             ))}
@@ -41,22 +45,22 @@ const DenseDisplay = (props: DenseDisplayProps<string>) => {
       return (
         <table className="w-full">
           <tbody className="flex flex-wrap">
-            {Object.entries(props.data).map(([key, value]) => (
+            {Object.entries(props.keys).map(([key, value]) => (
               <tr
-                className="flex grow-0 lg:basis-1/2 sm:basis-full border-b-[1px]"
                 key={key}
+                className="flex grow-0 lg:basis-1/2 sm:basis-full border-b-[1px]"
               >
                 <th
-                  data-testid="key"
+                  data-testid={"key_" + key}
                   className="lg:w-[200px] md:w-[200px] sm:w-[100px] text-left font-normal text-gray-500"
                 >
-                  {key}
+                  {value}
                 </th>
                 <td
-                  data-testid="value"
+                  data-testid={"value_" + key}
                   className="flex grow shrink text-left font-normal"
                 >
-                  {value}
+                  {props.values[key]}
                 </td>
               </tr>
             ))}
