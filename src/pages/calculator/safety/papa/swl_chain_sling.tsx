e98b078ebdae621a7698b44bf22a78f7a0f00628
Aza Maulana <azamaulanaaa@gmail.com>
2023-12-08 16:59:35 +0700

    fix(pages): missing page title from calculator swl chain sling

--- src/pages/calculator/safety/papa/swl_chain_sling.tsx
+++ src/pages/calculator/safety/papa/swl_chain_sling.tsx
@@ -1,9 +1,14 @@
-import SWLChainSlingCalculator from "@/features/calculator/safety/papa/swl_chain_sling";
+import Head from "next/head";
+
 import DashboardLayout from "@/layout/Dashboard";
+import SWLChainSlingCalculator from "@/features/calculator/safety/papa/swl_chain_sling";
 
 const SWLChainSling = () => {
   return (
     <DashboardLayout mobile>
+      <Head>
+        <title>SWL Chain Sling Calculator</title>
+      </Head>
       <div className="card card-bordered bg-base-100 shadow-md">
         <div className="card-body prose max-w-none">
           <h1>SWL Chain Sling Calculator</h1>
