ac82c0125723ee1705b0a0dcb0e775f0d40b6e46
Aza Maulana <azamaulanaaa@gmail.com>
2023-12-08 16:24:43 +0700

    fix(services): name conflict of supabase rexport type

--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -4,8 +4,8 @@ import {
 } from "@supabase/auth-helpers-nextjs";
 export type { Session, AuthError } from "@supabase/supabase-js";
 
-import { Database as DatabaseRaw } from "./supabase";
+import { Database as DatabaseSupabase } from "./supabase";
 
-export type DatabaseRaw = DatabaseRaw;
+export type DatabaseRaw = DatabaseSupabase;
 export type Database = SupabaseClient<DatabaseRaw>;
 export const newDatabase = createPagesBrowserClient;
