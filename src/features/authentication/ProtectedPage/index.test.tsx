49536dea957ea409225a3364f56e048ad72a5038
Aza Maulana <azamaulanaaa@gmail.com>
2023-12-08 15:27:32 +0700

    feat(features)!: Authentication ProtectedPage does not use loading

--- src/features/authentication/ProtectedPage/index.test.tsx
+++ src/features/authentication/ProtectedPage/index.test.tsx
@@ -25,16 +25,14 @@ describe("Protected Content Component", () => {
     const url = "http://localhost/";
 
     render(
-      <ProtectedPage hasAccess={true} redirectUrl={url} isReady={true}>
+      <ProtectedPage hasAccess={true} redirectUrl={url}>
         <div data-testid="child"></div>
       </ProtectedPage>,
     );
 
     const child = screen.queryByTestId("child");
-    const status = screen.queryByRole("status", { hidden: true });
 
     expect(child).toBeVisible();
-    expect(status).not.toBeVisible();
   });
 
   it("redirect to given url if has no access", async () => {
@@ -47,31 +45,12 @@ describe("Protected Content Component", () => {
     const url = "http://localhost/";
 
     render(
-      <ProtectedPage hasAccess={false} redirectUrl={url} isReady={true}>
+      <ProtectedPage hasAccess={false} redirectUrl={url}>
         <div data-testid="child"></div>
       </ProtectedPage>,
     );
 
     expect(router.push).toHaveBeenCalledWith(url);
     expect(router.push).toHaveBeenCalledTimes(1);
-  });
-
-  it("render loading animation if the access on verification", () => {
-    const router = {
-      isReady: true,
-    };
-    useRouter.mockReturnValue(router);
-
-    const url = "http://localhost/";
-
-    render(
-      <ProtectedPage hasAccess={false} redirectUrl={url} isReady={false}>
-        <div data-testid="child"></div>
-      </ProtectedPage>,
-    );
-
-    const loading = screen.getByRole("status", { hidden: true });
-
-    expect(loading).toBeVisible();
   });
 });
--- src/features/authentication/ProtectedPage/index.tsx
+++ src/features/authentication/ProtectedPage/index.tsx
@@ -1,26 +1,18 @@
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
 
-import Loading from "../Loading";
-
 export type ProtectedPageProps = {
   children: ReactNode;
   hasAccess: boolean;
   redirectUrl: string;
-  isReady: boolean;
 };
 
 const ProtectedPage = (props: ProtectedPageProps) => {
   const router = useRouter();
 
-  if (router.isReady && props.isReady && !props.hasAccess)
-    router.push(props.redirectUrl);
+  if (router.isReady && !props.hasAccess) router.push(props.redirectUrl);
 
-  return (
-    <Loading isLoading={!router.isReady || !props.isReady}>
-      {props.children}
-    </Loading>
-  );
+  return props.children;
 };
 
 export default ProtectedPage;
