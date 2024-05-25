8b9bc214654c76e23db11f97fd899c11318bcf0d
Aza Maulana <azamaulanaaa@gmail.com>
2024-05-25 13:44:16 +0700

    fix(calculator): req manual version for petir

--- src/pages/calculator/safety/electricity/petir_konvensional.tsx
+++ src/pages/calculator/safety/electricity/petir_konvensional.tsx
@@ -7,11 +7,11 @@ const Pinter = () => {
   return (
     <DashboardLayout mobile>
       <Head>
-        <title>Petir Konvensional</title>
+        <title>Petir Konvensional (ver.1)</title>
       </Head>
       <div className="card card-bordered bg-base-100 shadow-md">
         <div className="card-body prose max-w-none">
-          <h1>Petir Konvensional</h1>
+          <h1>Petir Konvensional (ver.1)</h1>
           <PetirKonvensionalCalculator />
         </div>
       </div>
