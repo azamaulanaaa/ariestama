ea13442d668b0dfee686e4d871f752d5b1d54fb4
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-14 19:20:37 +0700

    refactor(component): simplify how title work for calculator

--- src/component/calculator/mod.tsx
+++ src/component/calculator/mod.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, RefAttributes } from "react";
 import { z } from "zod";
 
 import { APAR } from "@/component/calculator/safety/apar/mod.tsx";
@@ -7,14 +7,15 @@ type InnerCalculatorProps = { locale: string };
 type InnerCalculator = <T extends InnerCalculatorProps>(props: T) => ReactNode;
 
 export enum CalculatorKind {
-  Apar_MinUnit = "Apar_MinUnit",
+  Apar_MinUnit = "APAR - Minimum Unit",
 }
 
-const InnerCalculatorMap: Record<CalculatorKind, [string, InnerCalculator]> = {
-  "Apar_MinUnit": ["APAR - Minimum Unit", APAR.MinUnit],
+const InnerCalculatorMap: Record<CalculatorKind, InnerCalculator> = {
+  [CalculatorKind.Apar_MinUnit]: APAR.MinUnit,
 };
 
 const CalculatorPropsSchema = z.object({
+  ref: z.custom<RefAttributes<HTMLDivElement>>().optional(),
   kind: z.enum(CalculatorKind),
   locale: z.string().optional().default("en-US"),
 });
@@ -23,15 +24,15 @@ export type CalculatorProps = z.input<typeof CalculatorPropsSchema>;
 export const Calculator = (props: CalculatorProps) => {
   const zProps = CalculatorPropsSchema.parse(props);
 
-  const [title, InnerCalculator] = InnerCalculatorMap[zProps.kind];
+  const InnerCalculator = InnerCalculatorMap[zProps.kind];
 
   return (
-    <div className="card bg-base-100 shadow-xl">
+    <div ref={zProps.ref} className="card bg-base-100 shadow-xl">
       <div className="card-body relative">
         <div className="absolute inset-0 z-0 opacity-10 rounded-2xl bg-[url('/img/logo.png')] bg-top">
         </div>
         <div className="relative z-1 prose max-w-none">
-          <h1>{title}</h1>
+          <h1>{zProps.kind}</h1>
           <InnerCalculator locale={zProps.locale} />
         </div>
       </div>
