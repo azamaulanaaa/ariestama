bd6a31755f068936ae11ae4d01b6cbf80eb84b70
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-14 17:46:14 +0700

    feat(hook): add useShareAsImage

---
+++ src/hook/useShareAsImage.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useToBlob } from "@hugocxl/react-to-image";
+
+export const useShareAsImage = <T extends HTMLElement>() => {
+  const [state, capture, captureRef] = useToBlob<T>();
+
+  useEffect(() => {
+    if (state.data && state.isSuccess) {
+      const file = new File([state.data], "image.png", { type: "image/png" });
+
+      if (navigator.canShare({ files: [file] })) {
+        navigator.share({ files: [file] }).catch((e) => {
+          if (e.name !== "AbortError") {
+            console.error("Sharing failed:", e);
+          }
+        });
+      } else {
+        console.warn(
+          "Web Share API for files is not supported on this device/browser.",
+        );
+      }
+    }
+  }, [state.isSuccess]);
+
+  return [captureRef, capture, state.isLoading];
+};
