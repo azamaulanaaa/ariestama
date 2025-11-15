811a6c70814d4e82b9911f9c866894e40a43bcbb
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-15 13:04:21 +0700

    feat(component): add boilder calculator stories

---
+++ src/component/calculator/boiler/mod.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, StoryDefault } from "@ladle/react";
+import { Boiler } from "./mod.tsx";
+
+export default {
+  title: "Component / Calculator / Boiler",
+} as StoryDefault;
+
+export const FireTubeTubeThickness: Story = () => (
+  <Boiler.FireTubeTubeThickness locale="id-ID" />
+);
+export const FireTubeShellThickness: Story = () => (
+  <Boiler.FireTubeShellThickness locale="id-ID" />
+);
+export const WaterTubeDrumThickness: Story = () => (
+  <Boiler.WaterTubeDrumThickness locale="id-ID" />
+);
+export const WaterTubeHoleThickness: Story = () => (
+  <Boiler.WaterTubeHoleThickness locale="id-ID" />
+);
