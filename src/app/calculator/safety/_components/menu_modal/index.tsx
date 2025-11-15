b531ad13820800271d82515d021df0308b694f89
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-15 14:45:15 +0700

    chore: remove app dir

--- src/app/calculator/safety/_components/menu_modal/index.tsx
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const MenuModalPropsSchema = z.object({}).optional();
-
-const MenuModal = (props: z.input<typeof MenuModalPropsSchema>) => {};
-
-export default MenuModal;
