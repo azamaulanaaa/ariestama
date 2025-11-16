488d96213c0479dda25ec875425001283c9e3872
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-16 13:34:47 +0700

    chore(component): rename file by removing water tube of tube hole

--- src/component/calculator/boiler/mod.tsx
+++ src/component/calculator/boiler/mod.tsx
@@ -1,4 +1,4 @@
-import { TubeHoleThickness } from "./water_tube_hole_thickness.tsx";
+import { TubeHoleThickness } from "./tube_hole_thickness.tsx";
 import { WaterTubeDrumThickness } from "./water_tube_drum_thickness.tsx";
 import { FireTubeShellThickness } from "./fire_tube_shell_thickness.tsx";
 import { FireTubeTubeThickness } from "./fire_tube_tube_thickness.tsx";
