0db509235589ff827d14a59dedbfc2e413deba66
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-16 13:51:28 +0700

    refactor(component): rename file for drum water tube thickness

--- src/component/calculator/boiler/mod.tsx
+++ src/component/calculator/boiler/mod.tsx
@@ -1,5 +1,5 @@
+import { DrumWaterTubeThickness } from "./drum_water_tube_thickness.tsx";
 import { TubeHoleThickness } from "./tube_hole_thickness.tsx";
-import { DrumWaterTubeThickness } from "./water_tube_drum_thickness.tsx";
 import { FireTubeShellThickness } from "./fire_tube_shell_thickness.tsx";
 import { FireTubeTubeThickness } from "./fire_tube_tube_thickness.tsx";
 
