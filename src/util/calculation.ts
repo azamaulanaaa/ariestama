928cbdb2abf11cade2fc9932fccf1a36f209e0d5
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-16 15:47:00 +0700

    refactor(util): move out girder from calculation file

--- src/util/calculation.ts
+++ src/util/calculation.ts
@@ -1,36 +1,5 @@
 import { z } from "zod";
 
-export const Girder = {
-  /**
-   * calculate max defelection given girder typee
-   *
-   * @param typee - type of girder could be one of ["single", "double"]
-   * @param lengthOfSpan - length of the span in mili meter
-   *
-   * @returns maximum deflection allowed in mili meter
-   */
-  deflection(typee: string, lengthOfSpan: number): number {
-    const zProps = z
-      .object({
-        typee: z.union([z.literal("single"), z.literal("double")]),
-        lengthOfSpan: z.number(),
-      })
-      .parse({
-        typee,
-        lengthOfSpan,
-      });
-
-    if (zProps.typee == "single") {
-      return (1 / 888) * zProps.lengthOfSpan;
-    }
-    if (zProps.typee == "double") {
-      return (1 / 600) * zProps.lengthOfSpan;
-    }
-
-    return NaN;
-  },
-};
-
 export const Boiler = {
   /**
    * Calculates the temperature-dependent stress reduction factor (f(Θ)) based on
--- src/util/calculation/mod.ts
+++ src/util/calculation/mod.ts
@@ -1,5 +1,6 @@
 import * as Chain from "./chain.ts";
 import * as Forklift from "./forklift.ts";
+import * as Girder from "./girder.ts";
 import * as Hydrant from "./hydrant.ts";
 import * as InstalasiPenyalurPetir from "./instalasi_penyalur_petir.ts";
 import * as Lingkungan from "./lingkungan.ts";
@@ -9,6 +10,7 @@ import * as TangkiTimbun from "./tanki_timbun.ts";
 export {
   Chain,
   Forklift,
+  Girder,
   Hydrant,
   InstalasiPenyalurPetir,
   Lingkungan,
