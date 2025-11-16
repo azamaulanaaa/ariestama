5659db63764cf620a58d69a9d2aa1ca2b8594594
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-16 14:08:13 +0700

    refactor(component): rename file for shell fire tube thickness

--- src/component/calculator/boiler/mod.tsx
+++ src/component/calculator/boiler/mod.tsx
@@ -1,6 +1,6 @@
 import { DrumWaterTubeThickness } from "./drum_water_tube_thickness.tsx";
 import { TubeHoleThickness } from "./tube_hole_thickness.tsx";
-import { ShellFireTubeThickness } from "./fire_tube_shell_thickness.tsx";
+import { ShellFireTubeThickness } from "./shell_fire_tube_thickness.tsx";
 import { FireTubeTubeThickness } from "./fire_tube_tube_thickness.tsx";
 
 export const Boiler = {
