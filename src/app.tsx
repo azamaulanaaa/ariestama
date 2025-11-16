b4344f12c4df3cb945a997bedb765d16cca0fe51
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-16 14:38:59 +0700

    fix(page): remove root page

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "@/app.css";
 
-import { Base } from "@/page/page.tsx";
 import { CalculatorPage } from "./page/calculator/safety/page.tsx";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Base />} />
         <Route path="calculator">
           <Route path="safety" element={<CalculatorPage />} />
         </Route>
--- src/page/page.tsx
+++
@@ -1,3 +0,0 @@
-export function Base() {
-  return <h1 className="badge badge-soft badge-primary">Hello, world!</h1>;
-}
