8caaf751b9320a241745cc668d5478159bc21df3
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-20 12:38:18 +0700

    feat(component): set max witdth for hydrant calculator stories

--- src/component/calculator/hydrant/mod.stories.tsx
+++ src/component/calculator/hydrant/mod.stories.tsx
@@ -10,5 +10,6 @@ export const DemoReservoir: Story<ReservoirProps> = (props) => (
 );
 DemoReservoir.storyName = "Reservoir";
 DemoReservoir.args = {
+  className: "max-w-100",
   locale: "id-ID",
 };
