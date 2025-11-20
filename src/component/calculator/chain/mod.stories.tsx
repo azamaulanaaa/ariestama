d2bdd0e538f6e46b772dc6deeda7ff3cf30c87c2
Aza Maulana <azamaulanaaa@gmail.com>
2025-11-20 11:01:50 +0700

    refactor(component): remove mod file of chain calculator

--- src/component/calculator/chain/mod.stories.tsx
+++ src/component/calculator/chain/mod.stories.tsx
@@ -1,9 +1,23 @@
 import { Story, StoryDefault } from "@ladle/react";
-import { Chain } from "./mod.tsx";
+import { SWLBlock, SWLBlockProps } from "./swl_block.tsx";
+import { SWLSling, SWLSlingProps } from "./swl_sling.tsx";
 
 export default {
   title: "Component / Calculator / Chain",
 } as StoryDefault;
 
-export const SwlBlock: Story = () => <Chain.SWLBlock locale="id-ID" />;
-export const SWLSling: Story = () => <Chain.SWLSling locale="id-ID" />;
+export const DemoSwlBlock: Story<SWLBlockProps> = (props) => (
+  <SWLBlock {...props} />
+);
+DemoSwlBlock.storyName = "SWL Block";
+DemoSwlBlock.args = {
+  locale: "id-ID",
+};
+
+export const DemoSwlSling: Story<SWLSlingProps> = (props) => (
+  <SWLSling {...props} />
+);
+DemoSwlSling.storyName = "SWL Sling";
+DemoSwlSling.args = {
+  locale: "id-ID",
+};
--- src/component/calculator/chain/mod.tsx
+++
@@ -1,7 +0,0 @@
-import { SWLSling } from "./swl_sling.tsx";
-import { SWLBlock } from "./swl_block.tsx";
-
-export const Chain = {
-  SWLSling,
-  SWLBlock,
-};
